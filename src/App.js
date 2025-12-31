1197ebc41c33b2a043d3fa4982eb9008f451e59c
KiranRaiChamling89 <kiranraichamling@outlook.com>
2025-12-31 16:06:10 +0530

    Revert "integrated firebase and its libraries, utility functions, signup and create user in firestore db"
    This reverts commit 60ae9916b54865930ef8abb928f11cc42dbfb116.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,12 @@
 
-import Home from './routes/home/home.component.routes';
-import { Routes, Route } from 'react-router-dom';
-import Navigation from './routes/navigation/navigation.component.routes';
-import SignIn from './routes/sign-in/sign-in.component.routes';
+import CategoryMenu from './components/category/category-menu/category-menu.component.jsx';
 
-const categories = [
-  {
-    id:1,
-    title:"Hat",
-    backgroundImage: "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    id:2,
-    title:"Jacket",
-    backgroundImage: "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    id:3,
-    title:"Sneakers",
-    backgroundImage: "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    id:4,
-    title:"Womens",
-    backgroundImage: "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    id:5,
-    title:"Mens",
-    backgroundImage: "https://i.ibb.co/R70vBrQ/men.png"
-  }
-];  
-const Shop = () => {
-  return <h1>I m at shop</h1>;
-};
-const App = () => { 
+const App = () => {
+  
   return (
-    <Routes>
-      <Route path='/' element={<Navigation /> } >
-        <Route index element = {<Home categories={categories} />} />
-        <Route path='/shop' element = {<Shop />} />
-        <Route path='/signIn' element = {<SignIn />} />
-      </ Route>
-    </Routes>
+    <main>
+      <CategoryMenu />
+    </main>
   );
 }
 
--- src/components/category/category-menu/category-menu.component.jsx
+++ src/components/category/category-menu/category-menu.component.jsx
@@ -1,7 +1,35 @@
 
 import CardList from '../category-item/category-item.component.jsx'
 import './category-menu.styles.scss';
-const CategoryMenu = ({categories}) => {
+const CategoryMenu = () => {
+    const categories = [
+    {
+      id:1,
+      title:"Hat",
+      backgroundImage: "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      id:2,
+      title:"Jacket",
+      backgroundImage: "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      id:3,
+      title:"Sneakers",
+      backgroundImage: "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      id:4,
+      title:"Womens",
+      backgroundImage: "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      id:5,
+      title:"Mens",
+      backgroundImage: "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ];
+
   return (
     <div className='categories-container'>
       {categories.map((category) => <CardList key={category.id} category={category} />)}
